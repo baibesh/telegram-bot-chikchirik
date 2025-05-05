2c06cac4a9fb69bf6df9c272f1ca1429aa54adba
Mukhtarkhan Baibesh <baibesh91@gmail.com>
2025-05-05 22:03:52 +0500

    fix: added config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,17 @@ module.exports = {
     {
       name: 'telegram-chikchirik-3000',
       script: 'dist/main.js',
-      instances: 'max',        // кластерный режим = все ядра
+      instances: 'max', // кластерный режим = все ядра
       exec_mode: 'cluster',
       env: { NODE_ENV: 'development', PORT: 3000 },
-      env_production: { NODE_ENV: 'production', PORT: 3000 }
-    }
-  ]
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+        ADMIN: process.env.ADMIN,
+        REDIS_HOST: process.env.REDIS_HOST,
+        REDIS_PORT: process.env.REDIS_PORT,
+      },
+    },
+  ],
 };
